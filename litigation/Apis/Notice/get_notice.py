444bafc35e8bcbdd5bb9b019e7949261264bbc47
SubodhBarche322 <subodhbarche@gmail.com>
2024-10-11 12:06:24 +0530

    GST Form Data in Masters data API

--- litigation/Apis/Notice/get_notice.py
+++ litigation/Apis/Notice/get_notice.py
@@ -1,14 +1,20 @@
 import frappe
 from frappe import _
 
-
-
-
-
 @frappe.whitelist()
+def test_function():
+    json_string = '{"name": "John", "age": 30}'
+    data = frappe.json.loads(json_string)
+    print(data)
+    return data, type(data)
+
+
+
+
+@frappe.whitelist(allow_guest=True)
 def get_master_data():
     try:
-  
+       
         entity_data = frappe.get_all('Entity Master', fields=['*'])
 
         
@@ -20,7 +26,10 @@ def get_master_data():
         
         user_data = frappe.get_all('User', fields=['name', 'email', 'role_profile_name'])
 
-   
+        gst_form_data = frappe.get_all('GST Form Master', 'form_name')
+
+
+        frappe.local.response['http_status_code'] = 200
         return {
             'status': 'success',
             'message': 'Data fetched successfully',
@@ -28,16 +37,23 @@ def get_master_data():
                 'entity_master': entity_data,
                 'department_master': department_data,
                 'issuing_authority_master': issuing_authority_data,
-                'user_data': user_data
+                'user_data': user_data,
+                'gst_form_data': gst_form_data
             }
         }
 
     except Exception as e:
-        
+      
+        frappe.local.response['http_status_code'] = 500
         return {
             'status': 'failure',
             'message': f'Error fetching data: {str(e)}'
         }
+
+
+
+
+
 
 
 # @frappe.whitelist()
